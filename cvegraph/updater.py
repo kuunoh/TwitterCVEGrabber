0b1a59abf9aac4c726d74a630858ae03b4b9a98e
kuunoh <yohan.dossantos90@laposte.net>
2021-03-25 13:45:22 +0100

    Updater set up to 6 hours

--- cvegraph/updater.py
+++ cvegraph/updater.py
@@ -6,6 +6,6 @@ from cvegraph.views import cve_chart_today
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(tweetgrabber.grab_tweet, 'interval', hours=1)
+    scheduler.add_job(tweetgrabber.grab_tweet, 'interval', hours=6)
     # scheduler.add_job(tweetgrabber.grab_tweet, 'interval', minutes=5)
     scheduler.start()
